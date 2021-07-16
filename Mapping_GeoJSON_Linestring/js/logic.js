f2efadee00dd3a725a8070709bb425da2ecedee5
RileyCC56 <rileycorpac@gmail.com>
2021-07-16 11:59:12 -0400

    code push

--- Mapping_GeoJSON_Linestring/js/logic.js
+++ Mapping_GeoJSON_Linestring/js/logic.js
@@ -5,7 +5,7 @@
 // let map = L.map('mapid').setView([30, 30], 2);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -20,14 +20,14 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Street: streets,
+  Street: light,
   Dark: dark
 };
 
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [30, 30],
+  center: [44.0, -80.0],
   zoom: 2,
   layers: [streets]
 })
@@ -39,9 +39,8 @@ L.control.layers(baseMaps).addTo(map);
 // //Then we add our streets tile layer
 // streets.addTo(map);
 
-
-// Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/RileyCC56/Mapping_Earthquakes/main/majorAirports.json"
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData =
 
 // // Grabbing our GeoJSON data.
 // d3.json(airportData).then(function(data) {
@@ -54,6 +53,7 @@ d3.json(airportData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
 L.geoJson(data, {
+  //adding popup with airport name
   onEachFeature: function(feature, layer) {
     console.log(layer);
     layer.bindPopup("<h3> Airport code: " + feature.properties.faa + "</h3> <hr><h3> Airport name: "
