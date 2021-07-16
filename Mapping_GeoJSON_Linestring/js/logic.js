40bd73f67b337b206d38bbfbd22cebd7fb872df6
RileyCC56 <rileycorpac@gmail.com>
2021-07-16 14:02:35 -0400

    polygon code push

--- Mapping_GeoJSON_Linestring/js/logic.js
+++ Mapping_GeoJSON_Linestring/js/logic.js
@@ -16,7 +16,7 @@ let night = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-guid
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
-
+});
 // Create a base layer that holds both maps.
 let baseMaps = {
   Light: day,
